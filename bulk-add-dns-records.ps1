cb166ab6cf6f617e1b4f8dbf48f111b279978e40
ehf <me@ehf.im>
2021-10-01 11:43:54 -0700

    Update bulk-add-dns-records.ps1

--- bulk-add-dns-records.ps1
+++ bulk-add-dns-records.ps1
@@ -1,4 +1,4 @@
-Import-CSV "C:\PS\NewDnsRecords.csv" | %{
+Import-CSV "C:\PS\example-bulk-dns-records.csv" | %{
 Add-DNSServerResourceRecordA -ZoneName example.com -Name $_."HostName" -CreatePtr -IPv4Address $_."IPAddress"
 }
 

37dc0a6a2e0a12529de184dbbebd9fc644679f64
ehf <me@ehf.im>
2021-10-01 13:56:31 -0700

    Update bulk-add-dns-records.ps1

--- dns/bulk-add-dns-records.ps1
+++ dns/bulk-add-dns-records.ps1
@@ -1,4 +1,11 @@
+Add-DnsServerPrimaryZone -ComputerName DC01 -NetworkId "10.10.10.0/24" -ReplicationScope Domain
+
+
 Import-CSV "C:\PS\example-bulk-dns-records.csv" | %{
 Add-DnsServerResourceRecordA -ZoneName example.com -Name $_."HostName" -CreatePtr -IPv4Address $_."IPAddress"
 }
+
+
+Get-DnsServerResourceRecord -ComputerName DC01 -ZoneName "10.10.10.in-addr.arpa"
+
 

a7821524fc64a0dd2f16f1ad599c71325f89e83e
ehf <me@ehf.im>
2021-10-04 20:45:00 -0700

    Create read-csv.ps1

---
+++ dns-prep/read-csv.ps1
@@ -0,0 +1,11 @@
+Import-Csv .\Documents\nwn-network-inventory-dns-prep-1.csv | ForEach-Object {
+    if  ( $($_.IPAddress) -match "^\d+") {
+        $str_pos = $($_.HostName).IndexOf('.')
+        if ( $str_pos -eq -1 ) {
+            $left_name = $($_.HostName).Trim().ToLower()
+        } else  {
+            $left_name = $($_.HostName.Substring(0, $str_pos)).Trim().ToLower()
+        }
+        Write-Host "$($_.IPAddress),$left_name.$($_.Domain_new)"
+    }
+}

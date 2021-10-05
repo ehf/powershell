e9a491aa4025bc6130f258e1fc03924875e78a46
ehf <me@ehf.im>
2021-10-04 18:17:14 -0700

    Update read-csv.ps1

--- dns-prep/read-csv.ps1
+++ dns-prep/read-csv.ps1
@@ -1,5 +1,5 @@
 
-Import-Csv .\Documents\nwn-network-inventory-dns-prep-1.csv | ForEach-Object {
+Import-Csv .\Documents\dns-prep.csv | ForEach-Object {
     if  ( $_.IPAddress -match "^\d+")
     {
         Write-Host "$($_.IPAddress),$($_.HostName).$($_.Domain_new)"
